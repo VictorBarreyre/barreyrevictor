023e69da8976548a49cc61164dbc46232c210524
VictorBarreyre <barreyrevictor.contact@gmail.com>
2023-12-09 19:40:23 +0100

    fourni

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,13 +11,19 @@ const Header = () => {
   const headerData = data[language].Header;
 
   const [ipAddress, setIpAddress] = useState('');
+  const [location, setLocation] = useState({ country: '', region: '' });
+  const [fourni, setFourni] = useState('')
   
 
   useEffect(() => {
-    fetch('https://api.ipify.org?format=json')
+    fetch('https://geo.ipify.org/api/v2/country?apiKey=at_Q5XWTrAQpqxoQibgOgqvSB93fkvK9')
       .then(response => response.json())
       .then(data => {  
         setIpAddress(data.ip);
+        setLocation({ country: data.location?.country, region: data.location?.region });
+        setFourni(data.isp)
+        console.log(data.isp)
+      
       })
       .catch(error => console.error('Erreur lors de la récupération de l’adresse IP:', error));
   }, []);
@@ -42,6 +48,8 @@ const Header = () => {
         </div>
         <div className='sub-info-sup'>
         <p className='sub-p'>{data[language].Button.settings.contentIP}{ipAddress} </p>
+        <p className='sub-p'>{data[language].Button.settings.contentLocation} {location.country}, {location.region} </p>
+        <p className='sub-p'>{data[language].Button.settings.contentFourni} {fourni}</p>
         <p className='sub-p'>{data[language].Button.settings.contentLang} {languageNames[language]} </p>
       </div>
   </div>
