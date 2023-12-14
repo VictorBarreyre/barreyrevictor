f64101e89148effcc92256073286131c8499072c
VictorBarreyre <barreyrevictor.contact@gmail.com>
2023-12-14 11:10:57 +0100

    tracè

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-
 const Loader = () => {
     const [textElements, setTextElements] = useState([]);
 
@@ -8,55 +7,53 @@ const Loader = () => {
         const elements = [];
         const screenWidth = window.innerWidth;
         const screenHeight = window.innerHeight;
-        const textWidth = 25; // Largeur approximative de chaque élément de texte
-        const textHeight = 25; // Hauteur approximative de chaque élément de texte
+        const textWidth = 25;
+        const textHeight = 25;
 
         const columns = Math.floor(screenWidth / textWidth);
         const rows = Math.floor(screenHeight / textHeight);
 
         for (let y = 0; y < rows; y++) {
             for (let x = 0; x < columns; x++) {
-                elements.push({ 
-                    id: `${y}-${x}`, 
+                elements.push({
+                    id: `${y}-${x}`,
                     style: {
                         left: `${x * textWidth}px`,
                         top: `${y * textHeight}px`
-                    } 
+                    }
                 });
             }
         }
         setTextElements(elements);
     }, []);
 
-    const handleTouchMove = (event) => {
-        const touches = event.touches[0];
-        if (touches) {
-            textElements.forEach((el, index) => {
-                const domNode = document.elementFromPoint(touches.clientX, touches.clientY);
-                if (domNode && domNode.key === `bv-text ${index}`) {
-                    setTextElements(prevElements =>
-                        prevElements.map((el, idx) => (idx === index ? { ...el, hidden: true } : el))
-                    );
-                }
-            });
-        }
-    };
-
-    const handleMouseOver = (id) => {
+    const handleInteraction = (id) => {
         setTextElements(prevElements =>
             prevElements.map(el => (el.id === id ? { ...el, hidden: true } : el))
         );
     };
 
     return (
-        <div className="text-container-loader">
+        <div
+            className="text-container-loader"
+            onTouchMove={(event) => {
+                const touch = event.touches[0];
+                const x = touch.clientX;
+                const y = touch.clientY;
+                const touchedElement = document.elementFromPoint(x, y);
+
+                if (touchedElement && touchedElement.dataset.id) {
+                    handleInteraction(touchedElement.dataset.id);
+                }
+            }}
+        >
             {textElements.map(el => (
                 <span
                     key={el.id}
+                    data-id={el.id}
                     className={`bv-text ${el.hidden ? 'hidden' : ''}`}
                     style={el.style}
-                    onMouseOver={() => handleMouseOver(el.id)}
-                    onTouchMove={handleTouchMove}
+                    onMouseOver={() => handleInteraction(el.id)}
                 >
                     BV/
                 </span>
@@ -66,4 +63,3 @@ const Loader = () => {
 };
 
 export default Loader;
-
